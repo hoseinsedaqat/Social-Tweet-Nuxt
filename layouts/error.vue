cab0e08df088f3788af0c083d243187370ee21a4
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-08-20 15:25:34 +0430

    Update error.vue

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,1 +1,15 @@
+<template>
+  <div>
+    <h3>This is Error Page</h3>
+    <nuxt-link to="/">Home</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "error",
+};
+</script>
+
+<style></style>
 
