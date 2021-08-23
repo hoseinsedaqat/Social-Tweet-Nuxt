2e6cfa34e72d4ba63faaf5ad5ee383540a2927ae
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-08-23 09:56:12 +0430

    Update index.js

--- store/index.js
+++ store/index.js
@@ -1,14 +1,1 @@
-import Vuex from 'vuex';
-
-const createStore = () => {
-    return new Vuex.Store({
-
-        state: {
-            allTweet: []
-        },
-
-        mutations: {
-            addTweet: (state, tweet) => {
-                state.allTweet.unshift(tweet);
-                localStorage.setItem('tweet', JSON.stringify(state.allTweet));
 
