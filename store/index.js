009cdcb80ebbdb5412a783da7d67ca55afe86bb2
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-08-23 09:56:52 +0430

    Update index.js

--- store/index.js
+++ store/index.js
@@ -1,1 +1,52 @@
+import Vuex from 'vuex';
 
+const createStore = () => {
+    return new Vuex.Store({
+
+        state: {
+            allTweet: []
+        },
+
+        mutations: {
+            addTweet: (state, tweet) => {
+                state.allTweet.unshift(tweet);
+                localStorage.setItem('tweet', JSON.stringify(state.allTweet));
+            },
+            saveTweet: (state) => {
+                if (localStorage.getItem('tweet')) {
+                    state.allTweet = JSON.parse(localStorage.getItem('tweet'));
+                }
+            },
+            deletedTweet: (state, id) => {
+                state.allTweet.splice(id, 1);
+                localStorage.setItem('tweet', JSON.stringify(state.allTweet));
+            },
+            changeTweetOne: (state, tweet) => {
+                var forChangeTweet = state.allTweet[Number(tweet.route) - 1];
+                if (forChangeTweet.id === tweet.id) {
+                    // of course for test and after redesign it
+                    console.log('true')
+                    forChangeTweet.tweet = tweet.tweet;
+                    localStorage.setItem('tweet', JSON.stringify(state.allTweet));
+                } else {
+                    // of course for test
+                    console.log('false')
+                }
+            }
+        },
+
+        actions: {
+            addTweet: ({ commit }, tweet) => {
+                commit('addTweet', tweet)
+            },
+            deletedTweet: ({ commit }, id) => {
+                commit('deletedTweet', id)
+            },
+            changeTweetOne: ({ commit }, tweet) => {
+                commit('changeTweetOne', tweet)
+            }
+        },
+    })
+}
+
+export default createStore;
