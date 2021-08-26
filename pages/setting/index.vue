b330a8d1e22582e433bef0d76979b44d3dc716bd
hsedaqat1378 <hoseinsedaqat6@gmail.com>
2021-08-26 10:24:18 +0430

    change class for setting h3 tag

--- pages/setting/index.vue
+++ pages/setting/index.vue
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-7 mx-auto py-4">
-          <h3>I have not Idea yet</h3>
+          <h3 class="display-5 text-dark">I have not Idea yet</h3>
         </div>
       </div>
     </div>
